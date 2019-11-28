3c49d8c3b4e57c78aedbfeb2ce74040da4d243c8
Carlos David A Arcos <carlos9_cv@hotmail.com>
2019-11-28 18:04:31 -0300

    Adição dos dados do WS

--- src/app/dashboard2/dashboard2.component.ts
+++ src/app/dashboard2/dashboard2.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartDataSets, ChartOptions } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
+import { WebSocketService } from '../services/websocket.service';
 
 @Component({
   selector: 'app-dashboard2',
   templateUrl: './dashboard2.component.html',
-  styleUrls: ['./dashboard2.component.css']
+  styleUrls: ['./dashboard2.component.css'],
+  providers: [WebSocketService]
 })
 export class Dashboard2Component implements OnInit {
 
-  lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June'];
+  constructor(private wsService: WebSocketService){
+    this.wsService.connect('ws://localhost:8080/quotes')
+      .subscribe(
+        (msg) => {
+          console.log('Mensagem recebida do WS: ' + msg.data);
+        },
+        (error) => {
+          console.log('Erro do WS: ' + error);
+        }
+      )
+  }
+
+  lineChartLabels: Label[] = ['10:20', '10:40', '10:50', '10:59', '11:09', '11:1'];
 
   lineChartOptions = {
     responsive: true,
@@ -17,8 +31,8 @@ export class Dashboard2Component implements OnInit {
 
   lineChartColors: Color[] = [
     {
-      borderColor: 'black',
-      backgroundColor: 'rgba(255,255,0,0.28)',
+      borderColor: 'yellow',
+      backgroundColor: 'rgba(255,255,0,0.15)',
     },
   ];
 
@@ -27,10 +41,8 @@ export class Dashboard2Component implements OnInit {
   lineChartType = 'line';
 
   lineChartData: ChartDataSets[] = [
-    { data: [85, 72, 78, 75, 77, 75], label: 'Crude oil prices' },
+    { data: [85, 72, 78, 75, 77, 75], label: 'Preços das Ações' },
   ];
-
-  constructor() { }
 
   ngOnInit(): void {
     console.log('DASH2');
